963db6737df83caf6b59e09f5614d8dac952c232
Graeme Smecher <gsmecher@t0.technology>
2025-07-11 15:19:10 -0700

    setenv.sh: adds autobuild alias

--- setenv.sh
+++ setenv.sh
@@ -4,3 +4,5 @@ export TEMPLATECONF=$ROOT/meta-t0-crs/conf/templates/t0-crs
 export PATH=$PATH:$ROOT/meta-xilinx/meta-xilinx-core/gen-machine-conf
 
 source $ROOT/poky/oe-init-build-env "$@"
+
+alias go_rootfs="git push -f straylight:autobuild HEAD:rootfs"
