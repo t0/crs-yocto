bd46bad1ed063865c172f73c9a3246c3b1f793ee
Graeme Smecher <gsmecher@t0.technology>
2026-02-02 10:51:22 -0800

    Use direnv to configure build environment

--- setenv.sh
+++ setenv.sh
@@ -3,6 +3,6 @@ export ROOT=$(readlink -f $(dirname "$BASH_SOURCE"))
 export TEMPLATECONF=$ROOT/meta-t0-crs/conf/templates/t0-crs
 export PATH=$PATH:$ROOT/meta-xilinx/meta-xilinx-core/gen-machine-conf
 
-source $ROOT/poky/oe-init-build-env "$@"
+source $ROOT/poky/oe-init-build-env build
 
 alias go_rootfs="git push -f straylight:autobuild HEAD:rootfs"
