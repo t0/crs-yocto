b8b37db6a88c1230d8d039fd6c0a7be1e5bd1b92
Graeme Smecher <gsmecher@t0.technology>
2026-06-29 09:47:40 -0700

    setenv.sh/hooks: grooms auto-build infrastructure
    There are associated Confluence pages:
    
        https://t0technology.atlassian.net/wiki/spaces/DH/pages/640778241
        https://t0technology.atlassian.net/wiki/spaces/DH/pages/1255964679
    
    that have been modified to match. This is an effort to make the
    Straylight build infrastructure broadly useful (trending towards
    "expected") rather than just something I do locally.

--- setenv.sh
+++ setenv.sh
@@ -5,4 +5,49 @@ export PATH=$PATH:$ROOT/meta-xilinx/meta-xilinx-core/gen-machine-conf
 
 source $ROOT/poky/oe-init-build-env build
 
-alias go_rootfs="git push -f straylight:autobuild HEAD:rootfs"
+BUILD_SERVER=${BUILD_SERVER:-straylight}
+BUILD_BASE=${BUILD_BASE:-autobuild}
+
+build-rootfs() {
+    git push -f $BUILD_SERVER:$BUILD_BASE HEAD:rootfs
+}
+
+fetch-collateral() {
+    # usage: fetch-collateral <collateral> <hash>
+    #
+    # When hash is not provided (which is probably the most ergonomically
+    # plausible case), this fetches the most recent successfully built
+    # collateral, so it will reach past failed builds. Because the filename has
+    # the hash embedded in it, this situation should be fairly noticeable.
+    local path hash
+    path=$(ssh "$BUILD_SERVER" \
+        "cd $BUILD_BASE/autobuild && ls -td ${2}*-rootfs/$1 2>/dev/null | head -n1") || return
+    if [ -z "$path" ]; then
+	    echo "fetch-wic: no collateral matching $1 for hash ${2:-(unset)} found" >&2
+        return 1
+    fi
+    hash="${path%%-rootfs/*}"
+    scp -p "$BUILD_SERVER:$BUILD_BASE/autobuild/$path" "${hash}-t0-crs-image-t0-crs.wic"
+}
+
+fetch-dcp() {
+    fetch-collateral 'build/tmp/work/cortexa72-cortexa53-poky-linux/crs-rfmux-bitstream/*/tcl/hw/hw.runs/impl_1/bd_wrapper_routed.dcp' $1
+}
+
+fetch-wic() {
+    fetch-collateral 'build/tmp/deploy/images/t0-crs/t0-crs-image-t0-crs.wic' $1
+}
+
+fetch-uboot() {
+    fetch-collateral 'build/tmp/deploy/images/t0-crs/boot.bin' $1
+}
+
+fetch-fsbl() {
+    fetch-collateral 'build/tmp/deploy/images/t0-crs/fsbl-t0-crs.elf' $1
+}
+
+fetch-release() {
+    fetch-wic
+    fetch-uboot
+    fetch-fsbl
+}
